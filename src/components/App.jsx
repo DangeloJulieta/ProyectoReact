9201ef975eebfe4ebb12afa3f1bc8f67bd87955e
Julieta D'Angelo <julietadangelo97@hotmail.com>
2023-02-01 01:29:04 -0300

    correcciones

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 //Components
 import { Navbar } from './Navbar/Navbar';
+import { ItemCount } from './ItemCount/ItemCount';
+import { ItemListContainer } from './ItemListContainer/ItemListContainer';
 
 export const App = () => {
   return (
   <>
     <Navbar nombre={" "}/>
+    <ItemCount ValInicial={0} stock={15}/>
+    <ItemListContainer greeting={"Prox productos"}/>
   </>
   )
 }
